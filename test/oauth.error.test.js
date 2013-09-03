5e6034b1a0889e43202e9a095a74e8f675a7dac9
Jared Hanson <jaredhanson@gmail.com>
2013-09-03 08:39:08 -0700

    Invoke verify callback inside try/catch.

--- test/oauth.error.test.js
+++ test/oauth.error.test.js
@@ -203,6 +203,60 @@ describe('OAuthStrategy', function() {
     });
   });
   
+  describe('that encounters a thrown error during verification', function() {
+    var strategy = new OAuthStrategy({
+        requestTokenURL: 'https://www.example.com/oauth/request_token',
+        accessTokenURL: 'https://www.example.com/oauth/access_token',
+        userAuthorizationURL: 'https://www.example.com/oauth/authorize',
+        consumerKey: 'ABC123',
+        consumerSecret: 'secret'
+      }, function(token, tokenSecret, profile, done) {
+        throw new Error('something was thrown');
+      });
+    
+    // inject a "mock" oauth instance
+    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
+      if (token == 'hh5s93j4hdidpola' && tokenSecret == 'hdhd0244k9j7ao03' && verifier == 'hfdp7dh39dks9884') {
+        return callback(null, 'nnch734d00sl2jdk', 'pfkkdhi9sl3r4s00', {});
+      } else {
+        return callback(null, 'wrong-token', 'wrong-token-secret');
+      }
+    }
+    
+    describe('handling an authorized callback request', function() {
+      var request
+        , info;
+
+      before(function(done) {
+        chai.passport(strategy)
+          .error(function(e) {
+            err = e;
+            done();
+          })
+          .req(function(req) {
+            request = req;
+            req.query = {};
+            req.query['oauth_token'] = 'hh5s93j4hdidpola';
+            req.query['oauth_verifier'] = 'hfdp7dh39dks9884';
+            req.session = {};
+            req.session['oauth'] = {};
+            req.session['oauth']['oauth_token'] = 'hh5s93j4hdidpola';
+            req.session['oauth']['oauth_token_secret'] = 'hdhd0244k9j7ao03';
+          })
+          .authenticate();
+      });
+
+      it('should error', function() {
+        expect(err).to.be.an.instanceof(Error);
+        expect(err.message).to.equal('something was thrown');
+      });
+      
+      it('should remove token and token secret from session', function() {
+        expect(request.session['oauth']).to.be.undefined;
+      });
+    });
+  });
+  
   describe('used within an app that does not have session support', function() {
     var strategy = new OAuthStrategy({
         requestTokenURL: 'https://www.example.com/oauth/request_token',
