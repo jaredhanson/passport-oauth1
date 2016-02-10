fd9d749c4c181a06235541bd2fc0d2f27afd293f
Jared Hanson <jaredhanson@gmail.com>
2016-02-09 18:04:39 -0800

    Remove empty test file.

--- test/oauth.error.test.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai')
-  , OAuthStrategy = require('../lib/strategy')
-  , InternalOAuthError = require('../lib/errors/internaloautherror');
-
-
-describe('OAuthStrategy', function() {
-  
-});
